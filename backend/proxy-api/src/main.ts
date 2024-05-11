7691091419b86d9b989f7ae0c9df7eee8e24448c
ndbzika <chakcpps@gmail.com>
2024-05-11 15:50:24 -0300

    fix: adding cors

--- backend/proxy-api/src/main.ts
+++ backend/proxy-api/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   app.use(passport.initialize());
   app.use(passport.session());
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
     credentials: true,
   });
   await app.startAllMicroservices();
