447236aa5765e9682f16437600f54e87d62f6d0d
ndbzika <chakcpps@gmail.com>
2024-05-18 00:08:23 -0300

    fix: No logged header icons

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom"
 import { useAuth } from "./AuthProvider"
 
 export const Header = () => {
-  const {logoutAdmin} = useAuth();
+  const {logoutAdmin, isLoggedIn} = useAuth();
   return (
     <header className="w-full h-28 bg-green flex justify-between items-center px-20">
-      <Link to='/admin/dashboard'>
+      {isLoggedIn() && (<Link to='/admin/dashboard'>
         <HomeIcon size="64" className="text-white cursor-pointer" />
-      </Link>
+      </Link>)}
       <h1 className="text-5xl text-white">Refeitorio</h1>
-      <LogOutIcon size="64" className="text-white cursor-pointer" onClick={logoutAdmin} />
+      {isLoggedIn() && (<LogOutIcon size="64" className="text-white cursor-pointer" onClick={logoutAdmin} />)}
     </header>
   )
 }
